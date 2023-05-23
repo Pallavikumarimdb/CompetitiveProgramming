8f09e02fe043d95c3c3e31c81ca8986dfa7e4803
Pallavi <pallavilpmt1995@gmail.com>
2023-05-23 17:11:33 +0530

    Fibonacci Number

--- LeetCode(Java)/src/Recursion/FibonacciNumber.java
+++ LeetCode(Java)/src/Recursion/FibonacciNumber.java
@@ -1,23 +1,23 @@
 package Recursion;
 
-public interface FibonacciNumber {
-        public static void main(String[] args) {
-            System.out.println(fibonacci(10));
-        }
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(10));
+    }
 
-        public static int fibonacci(int n){
+    public static int fibonacci(int n){
 
         // Base condition
 //        if(n<2){
 //            return n;
 //        }
-            if(n==0 || n==1){
-                return n;
-            }
-                return fibonacci(n-1)+fibonacci(n-2);
-
+        if(n==0 || n==1){
+            return n;
         }
+        return fibonacci(n-1)+fibonacci(n-2);
 
     }
+
+}
 
 
