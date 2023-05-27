362daf06770463a0b13219f2982dab45ebfcdf0e
Pallavi <pallavilpmt1995@gmail.com>
2023-05-27 21:44:23 +0530

    Solutions of few recursion questions

--- GFG(Java)/src/Recursion/FirstUppercaseLetterinaString.java
+++ GFG(Java)/src/Recursion/FirstUppercaseLetterinaString.java
@@ -2,11 +2,14 @@ package Recursion;
 
 public class FirstUppercaseLetterinaString {
     public static void main(String[] args) {
-        String s="geekSforgeeKs";
+        String s = "geekSforgeeKs";
         firstUppercase(s, 0);
+        System.out.println(findUpper(s));
     }
 
-    public static void firstUppercase(String s, int index){
+
+    // method 1
+    public static void firstUppercase(String s, int index)  {
 
         if(index==s.length()){
             System.out.println("UpperCase letter not found");
@@ -18,4 +21,32 @@ public class FirstUppercaseLetterinaString {
         }
         firstUppercase(s, index+1);
     }
+
+
+    // method 2
+    public static String findUpper(String s) {
+        if(s.length()==0){
+            return "UpperCase letter not found";
+        }
+        if (Character.isUpperCase(s.charAt(0))) return s.substring(0, 1);
+        else return findUpper(s.substring(1, s.length()));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
