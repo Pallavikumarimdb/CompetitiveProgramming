f88f88ad5c9f0b16ce0b0a3892512ba89e8194f9
Pallavi <pallavilpmt1995@gmail.com>
2024-11-26 21:39:39 +0530

    Strivers sheet

--- 05-Striver-A2Z-DSA-Sheet-JAVA/01-Learn-the-basics/Lec 4-Know Basic Maths/_02_Reverse_Integer.java
+++ 05-Striver-A2Z-DSA-Sheet-JAVA/01-Learn-the-basics/Lec 4-Know Basic Maths/_02_Reverse_Integer.java
@@ -24,4 +24,48 @@ public class _02_Reverse_Integer {
         }
         return (int)finalNum;
     }
+
+    //---------------------- METHOD 2 -------------------------- //
+
+    public int reverseInteger(int x){
+        boolean isNegative = x<0;
+        x=Math.abs(x);
+
+        // Reverse the digits by converting to string
+
+        String reverseStr = new StringBuilder(String.valueOf(x)).reverse().toString();
+
+        try{
+            int n=Integer.parseInt(reverseStr);
+            return isNegative ? -1*n : n;
+        }catch (NumberFormatException e){
+            // Overflow occurred
+            return 0;
+        }
+
+        // O(n) time and space complexity
+
+    }
+
+//    ...................... BEST .............................................
+        public int reverseMeth3(int x) {
+            int num = Math.abs(x);  // Original number ka absolute value nikala
+
+            int rev = 0;  // Reversed number
+
+            while (num != 0) {
+                int ld = num % 10;  // Last digit nikala
+
+                // Overflow check
+                // Whatever will add (id) and multiply (i.e-10) first will divide from rev and check if it is still more. If so after doing + and * it will exceed MAX value.
+                if (rev > (Integer.MAX_VALUE - ld) / 10) {
+                    return 0;  // Agar overflow hua, toh 0 return kardo
+                }
+
+                rev = rev * 10 + ld;  // Reverse mein digit ko add kiya
+                num = num / 10;  // Last digit hata diya, next iteration ke liye
+            }
+
+            return (x < 0) ? (-rev) : rev;  // Original number ke sign ke hisaab se result diya
+        }
 }
